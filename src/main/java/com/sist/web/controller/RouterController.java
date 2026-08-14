eecb3020c8fac75b223a717cf3f8875da9305474
thdgpfla9839-ctrl <thdgpfla9839@gmail.com>
2026-08-14 14:43:44 +0900

    change 파일

--- src/main/java/com/sist/web/controller/RouterController.java
+++ src/main/java/com/sist/web/controller/RouterController.java
@@ -3,6 +3,7 @@ package com.sist.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // 화면만 변경해주는 역할
 @Controller // router 기능
@@ -13,5 +14,14 @@ public class RouterController {
 	{
 		model.addAttribute("main_html","main/home");
 		return "main/main";
+	}
+	
+	@GetMapping("/food/detail")
+	public String food_detail(@RequestParam("no") int no, Model model)
+	{
+		model.addAttribute("no",no);
+		model.addAttribute("main_html","food/detail");
+		return "main/main";
+
 	}
 }
